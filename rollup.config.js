9fc9566bbe6a18d621dc76be791812fb5521a30d
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2018-12-21 15:31:16 +0400

    Point module directly to src file

--- rollup.config.js
+++ rollup.config.js
@@ -6,16 +6,10 @@ import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-  ],
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+  },
   external: [
     'react',
     'react-dom',
